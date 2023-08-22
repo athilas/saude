69ed99835784c2e5bec0a5ec09f8955a7d3baa25
Athila Salles <39576921+athilas@users.noreply.github.com>
2023-08-22 13:09:49 -0300

    colocando condicional para aparecer o resultado

--- src/componemte/resultado/Resultado.js
+++ src/componemte/resultado/Resultado.js
@@ -2,19 +2,21 @@ import './resultado.css';
 
 function Resultado({nome}){
        return(
-        <main className='saida'>
-            <h2>Nome: {nome}</h2>
-            <span>
-                <h3>Sintomas</h3>
-                <ul>
-                    <li>Tosse</li>
-                    <li>Febre</li>
-                    <li>Dor no corpo</li>
-                </ul>
-                <h3>O que fazer</h3>
-                <p>Deve procuar o hospital mais proximo da sua residencia</p>
-            </span>
-        </main>
+              {nome.length > 0 && (
+                      <main className='saida'>
+                          <h2>Nome: {nome}</h2>
+                          <span>
+                              <h3>Sintomas</h3>
+                              <ul>
+                                  <li>Tosse</li>
+                                  <li>Febre</li>
+                                  <li>Dor no corpo</li>
+                              </ul>
+                              <h3>O que fazer</h3>
+                              <p>Deve procuar o hospital mais proximo da sua residencia</p>
+                          </span>
+                      </main>
+       )}
     );
 }
 
