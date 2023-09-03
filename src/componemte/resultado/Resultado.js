7cadce5f06e598d2070279463889f9a21075c246
Athila Salles <athilas@hotmail.com>
2023-09-03 13:09:40 -0300

    renderizaçao condicional

--- src/componemte/resultado/Resultado.js
+++ src/componemte/resultado/Resultado.js
@@ -22,29 +22,8 @@ function Resultado({resultado}){
 
         }
        return(
-<<<<<<< HEAD
         <>
-          
-            
-
         </>
-=======
-              {nome.length > 0 && (
-                      <main className='saida'>
-                          <h2>Nome: {nome}</h2>
-                          <span>
-                              <h3>Sintomas</h3>
-                              <ul>
-                                  <li>Tosse</li>
-                                  <li>Febre</li>
-                                  <li>Dor no corpo</li>
-                              </ul>
-                              <h3>O que fazer</h3>
-                              <p>Deve procuar o hospital mais proximo da sua residencia</p>
-                          </span>
-                      </main>
-       )}
->>>>>>> 69ed99835784c2e5bec0a5ec09f8955a7d3baa25
     );
 }
 
