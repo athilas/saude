25c349a463ae39c3d897efe3c77679f060817f27
Athila Salles <39576921+athilas@users.noreply.github.com>
2023-09-05 09:20:38 -0300

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -27,7 +27,7 @@ function Home(){
     const saida = busca(nome);
     function busca(dado){
         for(let i in data){
-            if (dado === data[i].nome) return data[i];
+            if (dado.toLowerCase() === data[i].nome) return data[i];
         }
         return data[0];
     }
